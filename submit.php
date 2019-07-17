a15083d2280f9e82cdba2f5a967bf44e91663438
Martin Urbanec <martin@urbanec.cz>
2019-07-17 21:13:48 +0200

    Small fixes

--- submit.php
+++ submit.php
@@ -1,15 +1,16 @@
 <?php
-$html = file_get_contents("response.html");
+$response = file_get_contents("mail_response.html");
 foreach ($_POST as $key => $value) {
-    $html = str_replace("@@" . $key . "@@", $value, $html);
+    $response = str_replace("@@" . $key . "@@", $value, $response);
 }
 
 // Forward the response!
 $headers = "From: Wikikonference 2019 <wikikonference@wikimedia.cz>\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-//mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $html, $headers);
+//mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $response, $headers);
 
 // Store the response!
+$html = str_replace("@@response@@", $response, file_get_contents('layout.html'));
 file_put_contents('responses/' . uniqid("response", true) . ".html", $html);
 ?>
 
