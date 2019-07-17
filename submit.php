6d2397a01bcfbc45d2eaf406fbd56b6e02362f80
Martin Urbanec <martin@urbanec.cz>
2019-07-17 21:01:47 +0200

    Send the response

--- submit.php
+++ submit.php
@@ -4,4 +4,8 @@ $html = file_get_contents("response.html");
 foreach ($_POST as $key => $value) {
     $html = str_replace("@@" . $key . "@@", $value, $html);
 }
-echo $html;
+
+// Forward the response!
+$headers = "From: Wikikonference 2019 <wikikonference@wikimedia.cz>\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $html, $headers);
