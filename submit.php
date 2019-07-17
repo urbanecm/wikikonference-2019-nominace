fee9f2004a8b561213603ffcdad5bd6c1fc653ed
Martin Urbanec <martin@urbanec.cz>
2019-07-17 20:58:27 +0200

    Fix key in submit.php

--- submit.php
+++ submit.php
@@ -2,6 +2,6 @@
 <?php
 $html = file_get_contents("response.html");
 foreach ($_POST as $key => $value) {
-    $html = str_replace($key, $value, $html);
+    $html = str_replace("@@" . $key . "@@", $value, $html);
 }
 echo $html;
