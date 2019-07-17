10882cfe14741efd2de4fef83310975bc1112a03
Martin Urbanec <martin@urbanec.cz>
2019-07-17 21:06:12 +0200

    Show something meaningful after submitting

--- submit.php
+++ submit.php
@@ -1,4 +1,3 @@
-<h1>Response</h1>
 <?php
 $html = file_get_contents("response.html");
 foreach ($_POST as $key => $value) {
@@ -9,3 +8,33 @@ foreach ($_POST as $key => $value) {
 $headers = "From: Wikikonference 2019 <wikikonference@wikimedia.cz>\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $html, $headers);
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        td, tr {
+            text-align: center;
+        }
+    </style>
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Wikikonference 2019</a>
+    </nav>
+
+    <h1>Vaše odpověď byla úspěšně zaznamenána.</h1>
+</body>
+</html>
