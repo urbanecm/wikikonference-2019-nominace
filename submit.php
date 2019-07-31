72e6a8dbd6fee1c32b1ed124e777498c0751e5a4
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-07-31 17:03:44 +0200

    Re-enable mailing
    to get info about when new response was saved

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ foreach ($_POST as $key => $value) {
 // Forward the response!
 $headers = "From: Wikikonference 2019 <wikikonference@wikimedia.cz>\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-//mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $response, $headers);
+mail("martin.urbanec@wikimedia.cz", "[Wikikonference 2019] Nominace témat a řečníků", $response, $headers);
 
 // Store the response!
 $html = str_replace("@@response@@", $response, file_get_contents('layout.html'));
